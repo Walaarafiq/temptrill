62f5799d5b5b598959a238779c251b818c8bc5a0
Walaarafiq <54413315+Walaarafiq@users.noreply.github.com>
2022-01-15 12:43:54 +0300

    init app

---
+++ src/views/home/Child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>child component  {{title}} {{age}} </h3>
+    <input type="text" v-model="myName">
+   <button @click="$emit('changeName',myName)" >click</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Child',
+    props: ['title','age'],
+    data (){
+        return {
+            myName : ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+   <h1>home page </h1>
+   <Child 
+     
+      @changeName ='wrrite'
+      :title ='walaa' 
+      age='14'
+    />
+   
+   </div>
+</template>
+
+<script>
+
+import Child from './Child.vue' 
+export default {
+  name: 'Home',
+  components: {
+     Child
+  },
+  data(){
+    return {
+      walaa: 'wa'
+    }
+  },
+  methods: {
+    wrrite (val){
+      console.log(val);
+
+    }
+  }
+ 
+  }
+
+</script>
